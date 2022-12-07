b9ae0d47545245808c34502bd989a87cc54bd456
Nitinb72 <nitinb314@gmail.com>
2022-12-07 12:41:51 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'typing';
+  randomtext= faker.hacker.phrase();
+  userInput= '';
+
+
+  onInput(event: Event){
+    this.userInput= (event.target as HTMLInputElement).value;
+    console.log(this.userInput)
+  }
+
+  compare(randomtext: string, userInput: string){
+    if(!userInput) return 'pending';
+
+    // return randomLetter === userInput ? 'correct':'incorrect'
+
+    if(randomtext === userInput){
+      return 'correct';
+    }else{
+      return 'incorrect';
+    }
+
+  }
 }
